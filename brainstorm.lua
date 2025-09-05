3991e1cadaeb6d70bc252fae24711718351705ab
jamesy-tech <jamesycreator@gmail.com>
2025-09-05 12:20:41 +1200

    Update brainstorm.lua

--- brainstorm.lua
+++ brainstorm.lua
@@ -1,1 +1,36 @@
 
+-- MBIS Film Studies Brainstorming/Ideas --
+
+local selectedOption = {
+    number = 2,
+    description = [[
+In what ways does The Lion King depict (show) the struggle between good and evil
+through its symbolism and use of light and darkness?
+    ]]
+}
+
+local brainstorm = {
+    
+    [1] = [[
+
+The Lion King shows symbolism and the use of light and darkness by changing
+the lighting in different scenes, depending on what is going on.
+For example, if a sad scene is happening, the lighting might change to a more
+dark or red-ish colour. Other uses of lighting include creating depth,
+improving the atmosphere, or guiding the viewer's attention within the movie.
+   
+]],
+
+    [2] = [[
+
+Another way The Lion King shows symbolism between good and evil is through
+the Pride Lands (light) versus the Elephant Graveyard (dark). The light areas
+represent safety, life, and order, while the dark areas represent danger,
+destruction, and evil.
+
+]]
+
+}
+
+print(brainstorm[1]);
+print(brainstorm[2]);
