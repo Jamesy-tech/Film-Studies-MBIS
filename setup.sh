99eda694d529de7dd0a23fcfb2681cf0aff21ac7
jamesy-tech <jamesycreator@gmail.com>
2025-09-05 12:24:03 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,19 @@
 
+#!/bin/bash
+
+# Usage: bash setup.sh
+# *Requires PHP
+
+# This is for running brainstorm.lua, or main.cpp if I used it in this project
+
+luafile="brainstorm.lua"
+
+if [ "$1" = "C++" ]; then
+    g++ main.cpp -o main
+    ./main
+elif [ "$1" = "lua" ]; then
+    lua "$luafile"
+else
+    lua "$luafile"
+fi
+
